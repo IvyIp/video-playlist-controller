d23897febf27b2c7ee2b652a98a12709939dcd5f
Ivy-PC\Ivy <hkmg038836@gmail.com>
2018-09-26 09:46:41 +0800

    add demo

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# video-controller
-A wrapper for controller HTML5 video and playlist.
+# js-video-playlist-controller
+A javascript video playlist contorller.
+This handle the play states of video in the play list.
+## Demo
+https://ivyip.github.io/videoPlayListController/
+
+## Getting Started
+
+Just include the libray.
+```
+<script src="/path/to/js-video-playlist-controller.js"></script>
+```
+Initialize the class as followed:
+```
+new videoPlaylist(<wrapper>, <List of Video>, <Options>);
+```
+Example
+```
+<div id="video-wrapper"></div>
+...
+<script>
+let PlayListController =
+new videoPlaylist("#video-wrapper", ["1.mp4", "2.mp4", "3.mp4"], {autoplay: false, loop:true});
+</script>
+```
+## Options
+You can try to override some default value by setting the options.
+
+| Option | Description | Default |
+| --- | --- | --- |
+| autoplay | Autoplay the video when the class is initialized | true |
+| loop | Whether the playlist will start to play again when the list finished | true |
+| randomList | Shuffle the playlist when the class is initialized or when the list is came to end | false |
+
+## Methods
+Some methods are provided for checking the status of the video.
+They can be called by PlayListController.<method>.
+| Method | Description | Return Example |
+| --- | --- | --- |
+| getIsPlaying() | Get the play State. | false |
+| getCurrentVideo() | Get the current video's information. | {index: 0, video: "video.mp4"} |
+| getRepeatCount() | Number of time the list has repeated | 2 |
+| play() | Continue to play| - |
+| pause() | Pause the video| - |
+
+## Events
+Events are emitted when certain criteria are met.
+They dispatched by the video wrapper.
+Event can be caught in this way by pure javascript:
+```
+document.querySelectorAll("#video-wrapper")[0].addEventListener('videoChanged', function (e) {
+    console.log(`Currnet video: index ${e.detail.index} , path: ${e.detail.video}`);
+});
+```
+| Event | Description | Data |
+| --- | --- | --- |
+| videoChanged | The event is triggered when the video is ended and the next video is about to play. | {e: { detail: { index: <index>, video: <video>} } } |
+| playListEnded | The event is triggered when the play list has came to the end. | - |
---
+++ src/js-video-playlist-controller.js
@@ -0,0 +1,103 @@
+class videoPlaylist {
+    constructor(element, playList, config) {
+        this.playList = [];
+        this.index = 0;
+        this.isPlaying = false;
+        this.video = null;
+        this.source = null;
+        this.ended = false;
+        this.repeatCount = 0;
+        this.wrapper = null;
+
+        let configDefault = {
+            autoplay: true,
+            loop: true,
+            randomList: false
+        };
+        this.config = Object.assign({}, configDefault, config);
+        let video = document.createElement('video');
+        if (document.querySelectorAll(element).length !== 1){
+            throw "Please make sure there is one wrapper only";
+        }
+        this.wrapper = document.querySelectorAll(element)[0];
+        this.video = this.wrapper.appendChild(video);
+
+        if (this.config.randomList === true){
+            this.playList = this.randomize(playList);
+        }else{
+            this.playList = playList;
+        }
+
+        this.source = document.createElement('source');
+        this.source.setAttribute('src', this.playList[0]);
+        this.video.appendChild(this.source);
+
+        if (this.config.autoplay  === true) {
+            this.play();
+        }else{
+            this.pause();
+        }
+        this.video.onended = () => {
+            this.updateIndexByNext();
+            if (this.ended === false) {
+                this.video.load();
+                this.play();
+            }else{
+                this.pause();
+            }
+        };
+    }
+    play(){
+        this.isPlaying = true;
+        this.source.setAttribute('src', this.playList[this.index]);
+        this.video.play();
+    }
+    pause(){
+        this.isPlaying = false;
+        this.video.pause();
+    }
+    updateIndexByNext(){
+        if ((this.index + 1) >= this.playList.length){
+            if (this.config.randomList === true) {
+                this.playList = this.randomize(this.playList);
+            }
+            this.index = 0;
+            if (this.config.loop === false){
+                this.ended = true;
+                this.wrapper.dispatchEvent(new CustomEvent('playListEnded'));
+            }
+            this.repeatCount++;
+        }else{
+            this.index++;
+        }
+        if (this.ended === false) {
+            this.wrapper.dispatchEvent(new CustomEvent('videoChanged', {detail: this.getCurrentVideo()}));
+        }
+    }
+
+    static randomize(array) {
+        /** This function is contributed by Blender in https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array **/
+        let counter = array.length;
+        while (counter > 0) {
+            let index = Math.floor(Math.random() * counter);
+            counter--;
+            let temp = array[counter];
+            array[counter] = array[index];
+            array[index] = temp;
+        }
+        return array;
+    }
+
+    getCurrentVideo(){
+        return {index: this.index, video: this.playList[this.index]}
+    }
+
+    getRepeatCount(){
+        return this.repeatCount;
+    }
+
+    getIsPlaying(){
+        return this.isPlaying;
+    }
+
+}
